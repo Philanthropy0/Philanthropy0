f6e1b98771d4e326a3e3f5968c480fb00b7dc357
Philanthropy0 <441047787@qq.com>
2024-03-07 23:13:02 +0800

    test

--- js/index.js
+++ js/index.js
@@ -26,29 +26,61 @@ function changesubtitle(){
 }
 
 var boxleft = document.getElementById('box-left')
+var boxright = document.getElementById('box-right')
+
 var target = document.getElementById('screen-box1')
-var target2 = document.getElementById('screen-1')
+
+var target1 = document.getElementById('screen-1')
+var target2 = document.getElementById('screen-2')
+var target3 = document.getElementById('screen-3')
 
 var k=1
 
 console.log(target)
 var boxright = document.getElementById('box-right')
 
-function changescreen(){
-	alert("点击")
-	// target.style.opacity = 0;
-	console.log(target2.style.backgroundImage)
-	var st = "num"+k+".jpg"
-	var ima = "url(Image/"+st+")"
-	// console.log(ima)
-	target2.style.backgroundImage = ima;
-}
-
-boxleft.onclick = changescreen
-
-boxright.onclick = function(){
+function changescreen1(){
+	
 	// alert("点击")
-	target.style.opacity = 1;
+	// target.style.opacity = 0;
+	// console.log(target1.style.backgroundImage)
+	
+	k = k-3
+	var st1 = "num"+k+".jpg"
+	var st2 = "num"+(k+1)+".jpg"
+	var st3 = "num"+(k+2)+".jpg"
+	
+	var ima1 = "url(Image/"+st1+")"
+	var ima2 = "url(Image/"+st2+")"
+	var ima3 = "url(Image/"+st3+")"
+	
+	// console.log(ima)
+	target1.style.backgroundImage = ima1;
+	target2.style.backgroundImage = ima2;
+	target3.style.backgroundImage = ima3;
 }
 
+boxleft.onclick = changescreen1
 
+boxright.onclick = changescreen2
+
+function changescreen2(){
+	
+	// alert("点击")
+	// target.style.opacity = 0;
+	// console.log(target1.style.backgroundImage)
+	
+	k = k+3
+	var st1 = "num"+k+".jpg"
+	var st2 = "num"+(k+1)+".jpg"
+	var st3 = "num"+(k+2)+".jpg"
+	
+	var ima1 = "url(Image/"+st1+")"
+	var ima2 = "url(Image/"+st2+")"
+	var ima3 = "url(Image/"+st3+")"
+	
+	// console.log(ima)
+	target1.style.backgroundImage = ima1;
+	target2.style.backgroundImage = ima2;
+	target3.style.backgroundImage = ima3;
+}
